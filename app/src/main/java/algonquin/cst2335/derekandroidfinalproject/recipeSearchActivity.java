deac2aac31b24fcb45cdbfbb5a48a41acd7e287a
Derek Longman <long0269@algonquinlive.com>
2023-11-21 14:00:32 -0500

    initial commit

---
+++ app/src/main/java/algonquin/cst2335/derekandroidfinalproject/recipeSearchActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.derekandroidfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recipeSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_search);
+    }
+}
