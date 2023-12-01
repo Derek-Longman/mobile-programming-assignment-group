d282794e77fed1744da23ef2f2f57c200c8f9829
Bruce Raynard <rayn0034@algonquinlive.com>
2023-12-01 16:39:28 -0500

    4th commit

--- app/src/main/java/algonquin/cst2335/DezerSong/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/DezerSong/MainActivity.java
@@ -18,10 +18,21 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
+import algonquin.cst2335.derekandroidfinalproject.recipeSearchActivity;
 import algonquin.cst2335.myapplication.databinding.ActivityMainBinding;
 import algonquin.cst2335.myapplication.databinding.DeezerSongBinding;
 
@@ -44,7 +55,7 @@ ActivityMainBinding binding;
         switch( item.getItemId() )
         {
             case R.id.recipe:
-             startActivity(new Intent());
+             startActivity(new Intent(this, recipeSearchActivity.class));
 
                 break;
             case R.id.Help:
@@ -52,6 +63,7 @@ ActivityMainBinding binding;
                 alert.setTitle("Info of this page")
                         .setMessage("This shows the song artist name and the song from that artist." +
                                 "Put a url of the artist you want in the searchbar and click on the button.")
+                        .setPositiveButton("Close", (dialog, which) -> {})
                         .create()
                         .show();
 
@@ -60,16 +72,15 @@ ActivityMainBinding binding;
 
         return true;
     }
-
+    RequestQueue queue = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding =ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        queue = Volley.newRequestQueue(this);
         setSupportActionBar(binding.toolbar);
-        setTitle("Deezer Song");
         songs = new ArrayList<>();
-        songs.add("Test");
         SharedPreferences prefs = getSharedPreferences("SongData", Context.MODE_PRIVATE);
         String search = prefs.getString("SongSearch", "");
         binding.editTextText.setText(search);
@@ -77,10 +88,37 @@ ActivityMainBinding binding;
 
 
 binding.button.setOnClickListener(click ->{
-    Toast.makeText(this,"Toast: "+ binding.editTextText.getText().toString(),Toast.LENGTH_SHORT).show();
-    editor.putString("SongSearch",binding.editTextText.getText().toString());
+    String text = binding.editTextText.getText().toString();
+
+    Toast.makeText(this,"Toast: "+ text,Toast.LENGTH_SHORT).show();
+    editor.putString("SongSearch",text);
     editor.apply();
+
+    try {
+        String stringURL = "https://api.deezer.com/search/artist/?q="+ URLEncoder.encode(text,"UTF-8");
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+                (response) -> {
+                    try {
+                        JSONArray songArray=response.getJSONArray("data");
+                        JSONObject song_obj = songArray.getJSONObject(0);
+                        String artist_name = song_obj.getString("name");
+                    songs.add(artist_name);
+                    myAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+        (error) -> {
+            Toast.makeText(this, "FAILED", Toast.LENGTH_LONG).show();
+        });
+        queue.add(request);
+    } catch (UnsupportedEncodingException e) {
+        throw new RuntimeException(e);
+    }
+
 });
+
         binding.RecyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.RecyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
